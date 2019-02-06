62519013faaf5affb173937ecd5c84649909337d
Rob Crittenden <rcritten@redhat.com>
2019-02-06 14:09:46 -0500

    Provide CA, DS and HTTP instances as part of the IPAPlugin class
    This is so we can avoid having multiple plugins creating their own
    instances, perhaps in different ways.

--- src/idmcheck/ipa/plugin.py
+++ src/idmcheck/ipa/plugin.py
@@ -1,24 +1,34 @@
 from ipalib import api
+from ipapython.ipaldap import realm_to_serverid
+from ipaserver.install import cainstance
+from ipaserver.install import dsinstance
+from ipaserver.install import httpinstance
+
 
 from idmcheck.core.plugin import Plugin, Registry
 
 
 class IPAPlugin(Plugin):
-    pass
+    def __init__(self, registry):
+        super(IPAPlugin, self).__init__(registry)
+        self.ca = cainstance.CAInstance(api.env.realm,
+                                        host_name=api.env.host)
+        self.http = httpinstance.HTTPInstance()
+        self.ds = dsinstance.DsInstance()
+        self.serverid = realm_to_serverid(api.env.realm)
 
 
 class IPARegistry(Registry):
     def initialize(self, framework):
         if not api.isdone('bootstrap'):
-            api.bootstrap(in_server=False,
+            api.bootstrap(in_server=True,
                           context='idmcheck',
                           log=None)
         if not api.isdone('finalize'):
             api.finalize()
-# the backend is only available if in_server=True which requires
-# the ipaserver package
-#        if not api.Backend.ldap2.isconnected():
-#            api.Backend.ldap2.connect()
+
+        if not api.Backend.ldap2.isconnected():
+            api.Backend.ldap2.connect()
 
 
 registry = IPARegistry()
