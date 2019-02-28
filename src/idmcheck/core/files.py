e625fc56d2b8ca20a021c38257ed52b2ce4d4ac5
Rob Crittenden <rcritten@redhat.com>
2019-02-28 18:14:24 -0500

    Abstract file reporting into a core method

---
+++ src/idmcheck/core/files.py
@@ -0,0 +1,67 @@
+import grp
+import os
+import pwd
+
+from idmcheck.core import constants
+from idmcheck.core.plugin import Result, Results
+
+
+class FileCheck:
+    """Generic check to validate permission and ownership of files
+
+       files is a tuple of tuples. Each tuple consists of:
+           (path, expected_perm, expected_owner, expected_group)
+
+       perm is in the form of a POSIX ACL: e.g. 0440, 0770.
+       Owner and group are names, not uid/gid.
+    """
+
+    def check(self):
+        results = Results()
+
+        for (path, owner, group, mode) in self.files:
+            stat = os.stat(path)
+            fmode = str(oct(stat.st_mode)[-4:])
+            key = '%s_mode' % path.replace('/', '_')
+            if mode != fmode:
+                result = Result(self, constants.WARNING, key=key,
+                                path=path, expected=mode,
+                                got=fmode,
+                                msg='Permissions of %s are %s and '
+                                'should be %s' % (path, fmode, mode))
+            else:
+                result = Result(self, constants.SUCCESS, key=key,
+                                path=path)
+            results.add(result)
+
+            fowner = pwd.getpwnam(owner)
+            key = '%s_owner' % path.replace('/', '_')
+            if fowner.pw_uid != stat.st_uid:
+                actual = pwd.getpwuid(stat.st_uid)
+                result = Result(self, constants.WARNING, key=key,
+                                path=path, expected=owner,
+                                got=actual.pw_name,
+                                msg='Ownership of %s is %s and should '
+                                    'be %s' %
+                                    (path, actual.pw_name, owner))
+            else:
+                result = Result(self, constants.SUCCESS, key=key,
+                                path=path)
+            results.add(result)
+
+            fgroup = grp.getgrnam(group)
+            key = '%s_group' % path.replace('/', '_')
+            if fgroup.gr_gid != stat.st_gid:
+                actual = grp.getgrgid(stat.st_gid)
+                result = Result(self, constants.WARNING, key=key,
+                                path=path, expected=group,
+                                got=actual.gr_name,
+                                msg='Group of %s is %s and should '
+                                    'be %s' %
+                                    (path, actual.gr_name, group))
+            else:
+                result = Result(self, constants.SUCCESS, key=key,
+                                path=path)
+            results.add(result)
+
+        return results
--- src/idmcheck/ipa/files.py
+++ src/idmcheck/ipa/files.py
@@ -1,96 +1,42 @@
-import grp
 import os
-import pwd
 
+from idmcheck.core.files import FileCheck
 from idmcheck.ipa.plugin import IPAPlugin, registry
 
-from idmcheck.core.plugin import Result, Results
-from idmcheck.core import constants
-
 from ipaplatform.paths import paths
+from ipapython.certdb import NSS_SQL_FILES
 
 from ipaserver.install import dsinstance
 
 
 @registry
-class IPAFileNSSDBCheck(IPAPlugin):
-    def check(self):
-        results = Results()
+class IPAFileNSSDBCheck(IPAPlugin, FileCheck):
 
-        databases = [
-            {
-                'dirname': dsinstance.config_dirname(self.serverid),
-                'files': [
-                    ('key4.db', 'dirsrv', 'root', '0640'),
-                    ('cert9.db', 'dirsrv', 'root', '0640'),
-                    ('pkcs11.txt', 'dirsrv', 'dirsrv', '0640'),
-                ]
-            },
-        ]
+    def collect_files(self, basedir, filelist, owner, group, perms):
+        for file in filelist:
+            self.files.append((os.path.join(basedir, file), owner, group,
+                              perms))
+
+    def check(self):
+        self.files = []
+
+        self.collect_files(dsinstance.config_dirname(self.serverid),
+                           NSS_SQL_FILES, 'dirsrv', 'root', '0640')
+
+        # There always has to be a special one. pkcs11.txt has a different
+        # group so pop off the auto-generated one and add a replacement.
+        old = (os.path.join(dsinstance.config_dirname(self.serverid),
+               'pkcs11.txt'), 'dirsrv', 'root', '0640')
+        self.files.remove(old)
+        new = (os.path.join(dsinstance.config_dirname(self.serverid),
+               'pkcs11.txt'), 'dirsrv', 'dirsrv', '0640')
+        self.files.append(new)
 
         if self.ca.is_configured():
-            databases.append(
-                {
-                    'dirname': paths.VAR_LIB_IPA,
-                    'files': [
-                        ('ra-agent.key', 'root', 'ipaapi', '0440'),
-                        ('ra-agent.pem', 'root', 'ipaapi', '0440'),
-                    ]
-                },
-            )
+            self.collect_files(paths.PKI_TOMCAT_ALIAS_DIR, NSS_SQL_FILES,
+                               'pkiuser', 'pkiuser', '0600')
 
-            databases.append(
-                {
-                    'dirname': paths.PKI_TOMCAT_ALIAS_DIR,
-                    'files': [
-                        ('key4.db', 'pkiuser', 'pkiuser', '0600'),
-                        ('cert9.db', 'pkiuser', 'pkiuser', '0600'),
-                        ('pkcs11.txt', 'pkiuser', 'pkiuser', '0600'),
-                    ]
-                },
-            )
+            self.files.append((paths.RA_AGENT_PEM, 'root', 'ipaapi', '0440'))
+            self.files.append((paths.RA_AGENT_KEY, 'root', 'ipaapi', '0440'))
 
-        for db in databases:
-            for (file, owner, group, mode) in db['files']:
-                path = os.path.join(db['dirname'], file)
-                stat = os.stat(path)
-                fmode = str(oct(stat.st_mode)[-4:])
-                key = '%s_mode' % path.replace('/', '_')
-                if mode != fmode:
-                    result = Result(self, constants.WARNING, key=key,
-                                    path=path, expected=mode,
-                                    got=fmode,
-                                    msg='Permissions of %s are %s and '
-                                    'should be %s' % (path, fmode, mode))
-                else:
-                    result = Result(self, constants.SUCCESS, key=key,
-                                    path=path)
-                fowner = pwd.getpwnam(owner)
-                key = '%s_owner' % path.replace('/', '_')
-                if fowner.pw_uid != stat.st_uid:
-                    actual = pwd.getpwuid(stat.st_uid)
-                    result = Result(self, constants.WARNING, key=key,
-                                    path=path, expected=owner,
-                                    got=actual.pw_name,
-                                    msg='Ownership of %s is %s and should '
-                                        'be %s' %
-                                        (path, actual.pw_name, owner))
-                else:
-                    result = Result(self, constants.SUCCESS, key=key,
-                                    path=path)
-                fgroup = grp.getgrnam(group)
-                key = '%s_group' % path.replace('/', '_')
-                if fgroup.gr_gid != stat.st_gid:
-                    actual = grp.getgrgid(stat.st_gid)
-                    result = Result(self, constants.WARNING, key=key,
-                                    path=path, expected=group,
-                                    got=actual.gr_name,
-                                    msg='Group of %s is %s and should '
-                                        'be %s' %
-                                        (path, actual.gr_name, group))
-                else:
-                    result = Result(self, constants.SUCCESS, key=key,
-                                    path=path)
-                results.add(result)
-
-        return results
+        return FileCheck.check(self)
