7c3a69ea7c5cf424f30ad56f384a0e2f3739c56d
Rob Crittenden <rcritten@redhat.com>
2019-03-29 09:30:06 -0400

    Add IPA version and API version to metadata collection

--- src/ipahealthcheck/meta/core.py
+++ src/ipahealthcheck/meta/core.py
@@ -6,6 +6,7 @@ import socket
 from ipahealthcheck.core import constants
 from ipahealthcheck.core.plugin import Result, duration
 from ipahealthcheck.meta.plugin import Plugin, registry
+from ipapython.version import VERSION, API_VERSION
 
 
 @registry
@@ -13,4 +14,6 @@ class MetaCheck(Plugin):
     @duration
     def check(self):
         yield Result(self, constants.SUCCESS,
-                     fqdn=socket.getfqdn(),)
+                     fqdn=socket.getfqdn(),
+                     ipa_version=VERSION,
+                     ipa_api_version=API_VERSION,)
