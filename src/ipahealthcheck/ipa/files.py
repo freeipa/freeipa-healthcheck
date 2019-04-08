81b485de17c703a4166ee5d0accaad39ea5ae26d
Rob Crittenden <rcritten@redhat.com>
2019-04-08 10:44:38 -0400

    Extend list of files to check including custodia, keytabs & tomcat

--- src/ipahealthcheck/ipa/files.py
+++ src/ipahealthcheck/ipa/files.py
@@ -2,16 +2,24 @@
 # Copyright (C) 2019 FreeIPA Contributors see COPYING for license
 #
 
+import logging
 import os
 
 from ipahealthcheck.core.files import FileCheck
 from ipahealthcheck.ipa.plugin import IPAPlugin, registry
 
+from ipalib import api, errors
+
 from ipaplatform.paths import paths
+from ipaplatform.constants import constants
 from ipapython.certdb import NSS_SQL_FILES
+from ipapython.dn import DN
 
 from ipaserver.install import dsinstance
 from ipaserver.install import krbinstance
+
+
+logger = logging.getLogger()
 
 
 @registry
@@ -46,6 +54,14 @@ class IPAFileNSSDBCheck(IPAPlugin, FileCheck):
 
 @registry
 class IPAFileCheck(IPAPlugin, FileCheck):
+    def dns_container_exists(self):
+        try:
+            self.conn.get_entry(DN(api.env.container_dns,
+                                api.env.basedn), [])
+        except errors.NotFound:
+            return False
+        return True
+
     def check(self):
         self.files = []
 
@@ -56,5 +72,39 @@ class IPAFileCheck(IPAPlugin, FileCheck):
         if krbinstance.is_pkinit_enabled():
             self.files.append((paths.KDC_CERT, 'root', 'root', '0644'))
             self.files.append((paths.KDC_KEY, 'root', 'root', '0600'))
+
+        if self.dns_container_exists():
+            self.files.append((paths.NAMED_KEYTAB,
+                              constants.NAMED_USER,
+                              constants.NAMED_GROUP, '0400'))
+            if os.path.exists(paths.IPA_DNSKEYSYNCD_KEYTAB):
+                self.files.append((paths.IPA_DNSKEYSYNCD_KEYTAB,
+                                  'root', constants.ODS_GROUP, '0440'))
+
+        self.files.append((paths.GSSAPI_SESSION_KEY,
+                          'root', 'root', '0600'))
+        self.files.append((paths.DS_KEYTAB,
+                           constants.DS_USER, constants.DS_GROUP, '0600'))
+        self.files.append((paths.IPA_CA_CRT, 'root', 'root', '0644'))
+        self.files.append((paths.IPA_CUSTODIA_KEYS, 'root', 'root', '0600'))
+
+        return FileCheck.check(self)
+
+
+@registry
+class TomcatFileCheck(IPAPlugin, FileCheck):
+    def check(self):
+        if not self.ca.is_configured():
+            logger.debug('CA is not configured, skipping')
+            self.files = []
+        else:
+            self.files = [
+                (paths.PKI_TOMCAT_PASSWORD_CONF,
+                 constants.PKI_USER, constants.PKI_GROUP, '0660'),
+                (paths.CA_CS_CFG_PATH,
+                 constants.PKI_USER, constants.PKI_GROUP, '0660'),
+                (os.path.join(paths.PKI_TOMCAT, 'server.xml'),
+                 constants.PKI_USER, constants.PKI_GROUP, '0660'),
+            ]
 
         return FileCheck.check(self)
