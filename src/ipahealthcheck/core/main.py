7a912184559eef3c762055d8b800261cb93bf8e5
Rob Crittenden <rcritten@redhat.com>
2019-11-18 15:13:44 -0500

    Convert running healthchecks into a class and add pre-check
    Add a pre-check so that dependencies can be checked per product.
    In the case of IPA verify that the server is installed otherwise
    bail out.

--- src/ipahealthcheck/core/main.py
+++ src/ipahealthcheck/core/main.py
@@ -6,12 +6,23 @@ from os import environ
 import sys
 
 from ipahealthcheck.core import constants
-from ipahealthcheck.core.core import run_healthcheck
+from ipahealthcheck.core.core import RunChecks
+
+from ipaserver.install.installutils import is_ipa_configured
+
+
+class IPAChecks(RunChecks):
+    def pre_check(self):
+        if not is_ipa_configured():
+            print("IPA is not configured")
+            return 1
 
 
 def main():
     environ["KRB5_CLIENT_KTNAME"] = "/etc/krb5.keytab"
     environ["KRB5CCNAME"] = "MEMORY:"
 
-    sys.exit(run_healthcheck(['ipahealthcheck.registry'],
-                             constants.CONFIG_FILE))
+    ipachecks = IPAChecks(['ipahealthcheck.registry',
+                           'pkihealthcheck.registry'],
+                          constants.CONFIG_FILE)
+    sys.exit(ipachecks.run_healthcheck())
