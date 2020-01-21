dba6931cbfc5c733e3debe40938453bcadbf0933
Rob Crittenden <rcritten@redhat.com>
2020-01-21 14:14:50 -0500

    Read in all files, parse as JSON, skip invalid files
    Read in all files from the provided directory, verify that they
    are valid JSON, determine which host they are from and store the
    data for each host in a list stored in the registry (for now).
    
    There may be no complelling reasons to have multiple registries
    for cluster analysis, we'll see.

--- src/ipaclustercheck/ipa/plugin.py
+++ src/ipaclustercheck/ipa/plugin.py
@@ -2,7 +2,31 @@
 # Copyright (C) 2020 FreeIPA Contributors see COPYING for license
 #
 
+from copy import deepcopy
+import json
+import logging
+from os import listdir
+from os.path import isfile, join
+
 from ipahealthcheck.core.plugin import Plugin, Registry
+
+
+logger = logging.getLogger()
+
+def find_check(data, source, check):
+    """Look through the dict for a matching source and check.
+
+       data: dict of source and check output
+       source: name of source to find
+       check: name of check to find
+
+       Returns contents of source + check or None
+    """
+    for d in data:
+        if d.get('source') == source and d.get('check') == check:
+            return d
+
+    return None
 
 
 class ClusterPlugin(Plugin):
@@ -14,10 +38,39 @@ class ClusterRegistry(Registry):
     def initialize(self, framework, config):
         super(ClusterRegistry, self).initialize(framework, config)
 
+        self.json = {}
+
         self.load_files()
 
-    def load_files(self, dir='/tmp/clustercheck'):
-        pass
+    def load_files(self, dir='/tmp/hc'):
+        if self.json:
+            return
 
+        files = [f for f in listdir(dir) if isfile(join(dir, f))]
+        for file in files:
+            fname = join(dir, file)
+            logger.debug("Reading %s", fname)
+            try:
+                with open(fname, 'r') as fd:
+                    data = fd.read()
+            except IOError as e:
+                logger.error("Unable to read %s: %s", fname, e)
+                continue
+        
+            try:
+                data = json.loads(data)
+            except Exception as e:
+                logger.error("Unable to parse JSON in %s: %s", fname, e)
+                continue
 
+            meta = find_check(data, 'ipahealthcheck.meta.core',
+                                   'MetaCheck')
+            if meta:
+                fqdn = meta.get('kw').get('fqdn')
+                self.json[fqdn] = deepcopy(data)
+            else:
+                logger.error("No fqdn defined in JSON in %s", fname)
+                continue
+        
+        
 registry = ClusterRegistry()
