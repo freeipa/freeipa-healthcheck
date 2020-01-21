af77cc1491decfa98cf2f2670e2df35606b1ad5f
Rob Crittenden <rcritten@redhat.com>
2020-01-21 14:16:40 -0500

    Check that there is exactly one CRL Manager defined in the cluster
    Loop through all the hosts, identify those who are configured as
    CRL manager, and report:
    
    - if 0 then error
    - if 1 the things are fine
    - if > 1 then error

--- src/ipaclustercheck/ipa/crlmanager.py
+++ src/ipaclustercheck/ipa/crlmanager.py
@@ -2,14 +2,30 @@
 # Copyright (C) 2019 FreeIPA Contributors see COPYING for license
 #
 
-from ipaclustercheck.ipa.plugin import ClusterPlugin, registry
+from ipaclustercheck.ipa.plugin import ClusterPlugin, registry, find_check
 from ipahealthcheck.core.plugin import Result, duration
 from ipahealthcheck.core import constants
 
 
 @registry
-class ClusterExpirationCheck(ClusterPlugin):
+class ClusterCRLManagerCheck(ClusterPlugin):
 
     @duration
     def check(self):
-        yield Result(self, constants.SUCCESS)
+        data = self.registry.json
+        crlmanagers = []
+
+        for fqdn in data.keys():
+             output = find_check(data[fqdn], 'ipahealthcheck.ipa.roles',
+                                 'IPACRLManagerCheck')
+             enabled = output.get('kw').get('crlgen_enabled')
+             if enabled:
+                 crlmanagers.append(fqdn)
+        if len(crlmanagers) == 0:
+            yield Result(self, constants.ERROR, error='No CRL Manager defined')
+        elif len(crlmanagers) == 1:
+            yield Result(self, constants.SUCCESS, crlmanager=crlmanagers[0])
+        else:
+            yield Result(self, constants.ERROR,
+                         crlmanager=','.join(crlmanagers),
+                         error='Multiple CRL Managers defined')
