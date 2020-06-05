ca0ce978f4f8910bdfbb0c7dc28ef27b92a9dca4
Rob Crittenden <rcritten@redhat.com>
2020-06-05 14:44:33 -0400

    Look for dangling private groups
    Users and private groups are supposed to be linked together by
    the mepmanaged plugin but we've seen occasions where this linkage
    is broken. Look for private groups that have no associated users.
    
    https://pagure.io/freeipa/issue/1749

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
             'ipadna = ipahealthcheck.ipa.dna',
             'ipadns = ipahealthcheck.ipa.idns',
             'ipafiles = ipahealthcheck.ipa.files',
+            'ipagroups = ipahealthcheck.ipa.groups',
             'ipahost = ipahealthcheck.ipa.host',
             'ipameta = ipahealthcheck.ipa.meta',
             'iparoles = ipahealthcheck.ipa.roles',
