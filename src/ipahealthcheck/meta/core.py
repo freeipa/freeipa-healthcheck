508f6987c40200a7653c7d2b525fb41e112fc118
Rob Crittenden <rcritten@redhat.com>
2020-07-15 16:54:20 -0400

    Report on FIPS status: enabled, disabled, inconsistent, etc.
    If fips-mode-setup is installed then we can check the status
    otherwise report the missing binary.
    
    The script provides 3 possible return values:
    0 enabled
    1 inconsistent
    2 disabled
    
    These are handled along with a catch-all "unknown"
    
    https://github.com/freeipa/freeipa-healthcheck/issues/105
    
    Signed-off-by: Rob Crittenden <rcritten@redhat.com>

--- src/ipahealthcheck/meta/core.py
+++ src/ipahealthcheck/meta/core.py
@@ -2,18 +2,52 @@
 # Copyright (C) 2019 FreeIPA Contributors see COPYING for license
 #
 
+import logging
+import os
 import socket
 from ipahealthcheck.core import constants
 from ipahealthcheck.core.plugin import Result, duration
 from ipahealthcheck.meta.plugin import Plugin, registry
+from ipapython import ipautil
 from ipapython.version import VERSION, API_VERSION
+from ipaplatform.paths import paths
+
+logger = logging.getLogger()
 
 
 @registry
 class MetaCheck(Plugin):
     @duration
     def check(self):
-        yield Result(self, constants.SUCCESS,
+
+        rval = constants.SUCCESS
+        if not os.path.exists(paths.FIPS_MODE_SETUP):
+            fips = "missing {}".format(paths.FIPS_MODE_SETUP)
+            logger.debug('%s is not installed, skipping',
+                         paths.FIPS_MODE_SETUP)
+        else:
+            try:
+                result = ipautil.run([paths.FIPS_MODE_SETUP,
+                                      '--is-enabled'],
+                                     capture_output=True,
+                                     raiseonerr=False,)
+            except Exception as e:
+                logger.debug('fips-mode-setup failed: %s', e)
+                fips = "failed to check"
+                rval = constants.ERROR
+            else:
+                logger.debug(result.raw_output.decode('utf-8'))
+                if result.returncode == 0:
+                    fips = "enabled"
+                elif result.returncode == 1:
+                    fips = "inconsistent"
+                elif result.returncode == 2:
+                    fips = "disabled"
+                else:
+                    fips = "unknown"
+
+        yield Result(self, rval,
                      fqdn=socket.getfqdn(),
+                     fips=fips,
                      ipa_version=VERSION,
                      ipa_api_version=API_VERSION,)
