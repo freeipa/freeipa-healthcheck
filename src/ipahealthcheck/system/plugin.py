af94da91931a25640440f06c25eef2997fa77d65
Stanislav Levin <slev@altlinux.org>
2020-11-06 19:53:14 +0300

    pylint: Fix W0235(useless-super-delegation)
    Signed-off-by: Stanislav Levin <slev@altlinux.org>

--- src/ipahealthcheck/system/plugin.py
+++ src/ipahealthcheck/system/plugin.py
@@ -6,8 +6,7 @@ from ipahealthcheck.core.plugin import Plugin, Registry
 
 
 class SystemPlugin(Plugin):
-    def __init__(self, registry):
-        super().__init__(registry)
+    pass
 
 
 class SystemRegistry(Registry):
