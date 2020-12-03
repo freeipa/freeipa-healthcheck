0905ecfc6ac42cc25a41d915896fba14c80b544f
Rob Crittenden <rcritten@redhat.com>
2020-12-03 15:22:36 -0500

    Remove deprecated options from setup.py
    Remove ‘pytest-runner’ from your ‘setup_requires’, preferably
    removing the setup_requires option.
    
    Remove ‘pytest’ and any other testing requirements from
    ‘tests_require’, preferably removing the setup_requires option.
    
    We already use tox for basic environment setup.
    
    https://github.com/freeipa/freeipa-healthcheck/issues/161
    
    Signed-off-by: Rob Crittenden <rcritten@redhat.com>

--- setup.py
+++ setup.py
@@ -81,6 +81,4 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     python_requires='!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
-    setup_requires=['pytest-runner',],
-    tests_require=['pytest',],
 )
