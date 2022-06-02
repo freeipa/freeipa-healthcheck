89d1736c39efc3f25845f6ee42cfec65e97ba10a
Rob Crittenden <rcritten@redhat.com>
2022-06-02 11:11:31 -0400

    Become 0.11
    Signed-off-by: Rob Crittenden <rcritten@redhat.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='ipahealthcheck',
-    version='0.10',
+    version='0.11',
     namespace_packages=['ipahealthcheck', 'ipaclustercheck'],
     package_dir={'': 'src'},
     # packages=find_packages(where='src'),
