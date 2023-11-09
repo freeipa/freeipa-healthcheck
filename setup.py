a6b89d4823de1a3459f1189a7c9eb4fb1a9931b7
Rob Crittenden <rcritten@redhat.com>
2023-11-09 10:44:11 -0500

    Become 0.16

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='ipahealthcheck',
-    version='0.15',
+    version='0.16',
     namespace_packages=['ipahealthcheck', 'ipaclustercheck'],
     package_dir={'': 'src'},
     # packages=find_packages(where='src'),
