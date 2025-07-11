07a7ffebe82ac56be1f360b2868207a9f8f648cd
Rob Crittenden <rcritten@redhat.com>
2025-07-11 16:44:38 +0000

    Check that allowed_uids in the SSSD config is valid
    If UID 389 or user dirsrv is found in 'allowed_uids' in sssd.conf
    section [pac] then this will prevent SSSD from resolving the LDAP
    service account locally and can cause IPA services to fail.
    
    Fixes: https://github.com/freeipa/freeipa-healthcheck/issues/362
    
    Signed-off-by: Rob Crittenden <rcritten@redhat.com>

--- tests/test_ipa_config.py
+++ tests/test_ipa_config.py
@@ -1,12 +1,48 @@
 #
-# Copyright (C) 2025 FreeIPA Contributors see COPYING for license
+# Copyright (C) 2024 FreeIPA Contributors see COPYING for license
 #
 
 from util import capture_results, m_api
 from base import BaseTest
+from unittest.mock import patch
 from ipahealthcheck.core import config, constants
 from ipahealthcheck.ipa.plugin import registry
-from ipahealthcheck.ipa.config import IPAkrbLastSuccessfulAuth
+from ipahealthcheck.ipa.config import (
+    IPAkrbLastSuccessfulAuth,
+    SSSDAllowedUids389Check
+)
+
+from SSSDConfig import NoOptionError
+from SSSDConfig import NoServiceError
+
+
+class SSSDService():
+    def __init__(self, return_option, uids):
+        self.uids = uids
+        self.return_option = return_option
+
+    def get_option(self, option):
+        if not self.return_option:
+            raise NoOptionError
+        return self.uids
+
+
+class SSSDConfig():
+    def __init__(self, return_service=True, return_option=False, uids=None):
+        """
+        Knobs to control what data the configuration returns.
+        """
+        self.return_service = return_service
+        self.return_option = return_option
+        self.uids = uids
+
+    def import_config(self):
+        pass
+
+    def get_service(self, service):
+        if not self.return_service:
+            raise NoServiceError()
+        return SSSDService(self.return_option, self.uids)
 
 
 class TestkrbLastSuccessfulAuth(BaseTest):
@@ -52,3 +88,107 @@ class TestkrbLastSuccessfulAuth(BaseTest):
         assert result.result == constants.WARNING
         assert result.source == 'ipahealthcheck.ipa.config'
         assert result.check == 'IPAkrbLastSuccessfulAuth'
+
+
+class TestSSSDAllowedUids389Check(BaseTest):
+
+    @patch('SSSDConfig.SSSDConfig')
+    def test_sssd_no_pac_section(self, mock_sssd):
+        """There is no pac section in sssd.conf"""
+        mock_sssd.return_value = SSSDConfig(return_service=False,
+                                            return_option=False)
+        framework = object()
+        registry.initialize(framework, config.Config())
+        f = SSSDAllowedUids389Check(registry)
+        self.results = capture_results(f)
+
+        assert len(self.results) == 0
+
+    @patch('SSSDConfig.SSSDConfig')
+    def test_sssd_no_allowed_uids_configured(self, mock_sssd):
+        """There is no allowed_uids option in the pac section"""
+        mock_sssd.return_value = SSSDConfig(return_service=True,
+                                            return_option=False)
+        framework = object()
+        registry.initialize(framework, config.Config())
+        f = SSSDAllowedUids389Check(registry)
+        self.results = capture_results(f)
+
+        assert len(self.results) == 1
+        result = self.results.results[0]
+        assert result.result == constants.SUCCESS
+        assert result.source == 'ipahealthcheck.ipa.config'
+        assert result.check == 'SSSDAllowedUids389Check'
+
+    @patch('SSSDConfig.SSSDConfig')
+    def test_sssd_ok_allowed_uids_configured(self, mock_sssd):
+        """There is now allowed_uids option in the pac section"""
+        mock_sssd.return_value = SSSDConfig(return_service=True,
+                                            return_option=True,
+                                            uids='0')
+        framework = object()
+        registry.initialize(framework, config.Config())
+        f = SSSDAllowedUids389Check(registry)
+        self.results = capture_results(f)
+
+        assert len(self.results) == 1
+        result = self.results.results[0]
+        assert result.result == constants.SUCCESS
+        assert result.source == 'ipahealthcheck.ipa.config'
+        assert result.check == 'SSSDAllowedUids389Check'
+
+    @patch('SSSDConfig.SSSDConfig')
+    def test_sssd_ok_multiple_allowed_uids_configured(self, mock_sssd):
+        """There is now allowed_uids option in the pac section"""
+        mock_sssd.return_value = SSSDConfig(return_service=True,
+                                            return_option=True,
+                                            uids='0, 100000')
+
+        # uid 100000 is a value I picked out of the air. It doesn't
+        # matter what it is as it isn't prohibited
+        framework = object()
+        registry.initialize(framework, config.Config())
+        f = SSSDAllowedUids389Check(registry)
+        self.results = capture_results(f)
+
+        assert len(self.results) == 1
+        result = self.results.results[0]
+        assert result.result == constants.SUCCESS
+        assert result.source == 'ipahealthcheck.ipa.config'
+        assert result.check == 'SSSDAllowedUids389Check'
+
+    @patch('SSSDConfig.SSSDConfig')
+    def test_sssd_bad_allowed_uids_configured(self, mock_sssd):
+        """There is now allowed_uids option in the pac section"""
+        mock_sssd.return_value = SSSDConfig(return_service=True,
+                                            return_option=True,
+                                            uids='0, 389')
+        framework = object()
+        registry.initialize(framework, config.Config())
+        f = SSSDAllowedUids389Check(registry)
+        self.results = capture_results(f)
+
+        assert len(self.results) == 1
+        result = self.results.results[0]
+        assert result.result == constants.ERROR
+        assert result.kw.get('invalid') == '389'
+        assert result.source == 'ipahealthcheck.ipa.config'
+        assert result.check == 'SSSDAllowedUids389Check'
+
+    @patch('SSSDConfig.SSSDConfig')
+    def test_sssd_bad_alpha_allowed_uids_configured(self, mock_sssd):
+        """There is now allowed_uids option in the pac section"""
+        mock_sssd.return_value = SSSDConfig(return_service=True,
+                                            return_option=True,
+                                            uids='root, dirsrv')
+        framework = object()
+        registry.initialize(framework, config.Config())
+        f = SSSDAllowedUids389Check(registry)
+        self.results = capture_results(f)
+
+        assert len(self.results) == 1
+        result = self.results.results[0]
+        assert result.result == constants.ERROR
+        assert result.kw.get('invalid') == 'dirsrv'
+        assert result.source == 'ipahealthcheck.ipa.config'
+        assert result.check == 'SSSDAllowedUids389Check'
